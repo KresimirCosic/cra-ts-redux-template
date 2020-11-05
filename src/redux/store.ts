9dd5d5ad1338f89eaa4354c9857a470b2cddd2fc
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-11-05 09:24:56 +0100

    Redux slices Introduced some default slices for Redux store.

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import userInterfaceReducer from './slices/userInterface';
+import authenticationReducer from './slices/authentication';
+import SEOReducer from './slices/SEO';
+
+const store = configureStore({
+  reducer: {
+    userInterface: userInterfaceReducer,
+    authentication: authenticationReducer,
+    SEO: SEOReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
